a4769d124005bcf2143b655c2bd7da344dabfaa9
Tobias Stelzer <t.stelzer@fis-gmbh.de>
2016-11-22 13:49:08 +0000

    random

--- bewerbungverwaltung-cloud-web/src/main/webapp/controller/BewerbungDetail.controller.js
+++ bewerbungverwaltung-cloud-web/src/main/webapp/controller/BewerbungDetail.controller.js
@@ -1,31 +1,33 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/UIComponent"
-], function(Controller, UIComponent) {
+], function(Controller, UIComponent, History) {
 	"use strict";
-	
+
 	return Controller.extend("de.fis.bewerbungverwaltung.controller.BewerbungDetail", {
-		
-		onInit:function()
-		{
+
+		onInit: function() {
 			var oRouter = UIComponent.getRouterFor(this);
 
 			oRouter.getRoute("BewerbungDetail").attachPatternMatched(this._handleRouteMatched, this);
 		},
-		_handleRouteMatched: function (oEvent) {
+		_handleRouteMatched: function(oEvent) {
 			var sBindingEntity = "testModel>/Bewerbungs/" + oEvent.getParameter("arguments").Bewerbung;
 
 			this.getView().bindElement(sBindingEntity);
-//			this.getView().getModel("testModel").updateBindings(true);
+			//			this.getView().getModel("testModel").updateBindings(true);
+		},
+		onNavButtonPressed: function(oEvent) {
+			UIComponent.getRouterFor(this).navTo("BewerbungenMaster");
 		},
 		onExit: function() {
-			
+
 		},
 		onAfterRendering: function() {
-		
+
 		},
 		onBeforeRendering: function() {
-		
+
 		},
 		onSemanticButtonPress: function(oEvent) {
 			sap.m.MessageToast.show("onSemanticButtonPress");
@@ -39,7 +41,7 @@ sap.ui.define([
 		onPress: function(oEvent) {
 			sap.m.MessageToast.show("onPress");
 		}
-		
+
 	});
-	
+
 });
