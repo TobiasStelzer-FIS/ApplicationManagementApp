c449cbb661073eedab83fe726aafb8b409fcd515
Tobias Stelzer <t.stelzer@fis-gmbh.de>
2016-11-22 14:38:13 +0000

    Created custom-control (SimpleList)

---
+++ bewerbungverwaltung-cloud-web/src/main/webapp/control/SimpleList.js
@@ -0,0 +1,57 @@
+sap.ui.define([
+	"sap/ui/core/Control",
+	"sap/m/Text"
+], function(Control, Text) {
+	"use strict";
+
+	return Control.extend("de.fis.bewerbungverwaltung.control.SimpleList", {
+
+		metadata: {
+			properties: {
+				title: {
+					type: "String",
+					defaultValue: ""
+				}
+			},
+			aggregations: {
+				_items: {
+					multiple: true,
+					visibility: "public",
+					singularName: "item"
+				}
+			},
+			events: {}
+		},
+
+		init: function() {
+
+		},
+
+		renderer: function(oRm, oControl) {
+			oRm.write("<div");
+			oRm.writeControlData(oControl);
+			oRm.addClass("mySimpleList");
+			oRm.writeClasses();
+			oRm.write(">");
+
+			if (oControl.getTitle != "") {
+				var oTitle = new Text();
+				oTitle.setText(oControl.getTitle());
+				oRm.renderControl(oTitle);
+			}
+			
+			var aChildren = oControl.getItems();
+			oRm.write("<ul>");
+			for (var i = 0; i < aChildren.length; i++) { // loop over all child Controls, 
+				oRm.write("<li>");
+//				oRm.writeStyles();
+				oRm.renderControl(aChildren[i]);
+				oRm.write("</li>");
+			}
+			oRm.write("</ul>");
+			
+			oRm.write("</div>");
+		}
+	});
+
+});
